2a5e699aa41f172db1c2779072c7e283bf210ce2
Dhyghi <167600661+Dhyghi@users.noreply.github.com>
2024-05-25 13:39:50 +0300

    Create arthur-mc.js

---
+++ src/arthur-mc.js
@@ -0,0 +1,50 @@
+[
+{
+"link_song": "https://k.top4top.io/m_30427az9r0.mp3"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/cFgTBq5smuONwUoZnBBP4jKy3D12OtKc"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/YMc2EfKTab3oKVHc5vSl84FdtLlBNbyq"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/dSiD4xXyoiArvVGIlbMJVNWnI5UM3CxR"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/oKBHAMqduvHJKZr0m4GahELoHXx222r3"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/1IiYMCueOJFmCiPGbI1FGDcIvGh5ffRP"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/SErFliXRAV3iQntqBRhTH94u3vLApK7r"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/SErFliXRAV3iQntqBRhTH94u3vLApK7r"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/ESPWlVK7kvQstqkXTOwoiVtqG2X5T7tu"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/7G2W7bgkPCa6wRkEKRgJ5Wv7b6FTPH02"
+}, 
+{
+"link_song":"https://audio.jukehost.co.uk/fH73vhc5PXI3oCEOnqqo03G9rDK6iXfD"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/Rx1uYvvuF7b1lJg1zwucHaOCbVi1dQyi"
+}, 
+{
+"link_song": "https://audio.jukehost.co.uk/cEDaa7SpNcJSXCaoa1DhRpyLglXEhIms"
+}
+  
