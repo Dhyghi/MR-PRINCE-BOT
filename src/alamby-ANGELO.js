951705474966560fd3855a1bf6fc7412a1295a14
Dhyghi <167600661+Dhyghi@users.noreply.github.com>
2024-06-24 22:10:36 +0300

    Create alamby-ANGELO.js

---
+++ src/alamby-ANGELO.js
@@ -0,0 +1,178 @@
+[
+ {
+  "img": "https://telegra.ph/file/74aa472c89f57093b4d1e.jpg",
+ "name": "المغرب"
+    },
+    {
+  "img": "https://telegra.ph/file/0b068eead9bb47bee6332.jpg",
+ "name": "العراق"
+    },
+    {
+  "img": "https://telegra.ph/file/989fba06d7f799bc9a7d9.jpg",
+ "name": "سوريا"
+    },
+    {
+  "img": "https://telegra.ph/file/53ecdffbad9571b8dda0f.jpg",
+ "name": "الاردن"
+    },
+    {
+  "img": "https://telegra.ph/file/0ab113a5eda703fd2ba59.jpg",
+ "name": "السعوديه"
+    },
+    {
+  "img": "https://telegra.ph/file/89f1d4249cfc19695fd2e.jpg",
+ "name": "اليمن"
+    },
+    {
+  "img": "https://telegra.ph/file/331652cdf5bf51e9234fd.jpg",
+ "name": "مصر"
+    },
+    {
+  "img": "https://telegra.ph/file/6ea2a535edc57db7a0064.jpg",
+ "name": "لبنان"
+    },
+    {
+  "img": "https://telegra.ph/file/72df536ecbb290143e048.jpg",
+ "name": "الجزائر"
+    },
+    {
+  "img": "https://telegra.ph/file/c13ac816b15bff98ead95.jpg",
+ "name": "الصومال"
+    },
+    {
+  "img": "https://telegra.ph/file/e14223c483a32e6374581.jpg",
+ "name": "ليبيا"
+    },
+    {
+  "img": "https://telegra.ph/file/dad981cdfde318faad67e.jpg",
+ "name": "عمان"
+    },
+    {
+  "img": "https://telegra.ph/file/d27c4ab73c89fb1d62a5b.jpg",
+ "name": "روسيا"
+    },
+    {
+  "img": "https://telegra.ph/file/4c984452a9509c295c33f.jpg",
+ "name": "اليابان"
+       },
+    {
+  "img": "https://telegra.ph/file/96b6436e3f6e3e00edc73.jpg",
+ "name": "الصين"
+       },
+    {
+  "img": "https://telegra.ph/file/f8c7f8f77a5923fab0956.jpg",
+ "name": "فرنسا"
+       },
+    {
+  "img": "https://telegra.ph/file/934bbdce94952189b28e3.jpg",
+ "name": "المانيا"
+       },
+    {
+  "img": "https://telegra.ph/file/063c214d43366283e03d7.jpg",
+ "name": "الولايات المتحده الامريكيه"
+       },
+    {
+  "img": "https://telegra.ph/file/dff3da371f50cc546fd70.jpg",
+ "name": "كندا"
+       },
+    {
+  "img": "https://telegra.ph/file/b93ab87b62ec582185016.jpg",
+ "name": "الهند"
+       },
+    {
+  "img": "https://telegra.ph/file/d4435643f43ac0326a305.jpg",
+ "name": "سويسرا"
+       },
+    {
+  "img": "https://telegra.ph/file/b48a2340c5c08ed562011.jpg",
+ "name": "البرازيل"
+       },
+    {
+  "img": "https://telegra.ph/file/b0473c8b8d41c628449c0.jpg",
+ "name": "الارجنتين"
+       },
+    {
+  "img": "https://telegra.ph/file/56e6c4338affd1aa54077.jpg",
+ "name": "البحرين"
+       },
+    {
+  "img": "https://telegra.ph/file/106729ad8440f28fc5e99.jpg",
+ "name": "اسبانيا"
+       },
+    {
+  "img": "https://telegra.ph/file/d1fac6dbe5ab48cc1c9bc.jpg",
+ "name": "الامارات"
+       },
+    {
+  "img": "https://telegra.ph/file/eae04554d5300eb28df91.jpg",
+ "name": "قطر"
+       },
+    {
+  "img": "https://telegra.ph/file/9588c35572404b43696a4.jpg",
+ "name": "ايطاليا"
+       },
+    {
+  "img": "https://telegra.ph/file/db0fac93bfe9b948904c5.jpg",
+ "name": "المملكه المتحده"
+       },
+    {
+  "img": "https://telegra.ph/file/9d41d19e90a0303e92d2a.jpg",
+ "name": "كوريا الجنوبيه"
+       },
+    {
+  "img": "https://telegra.ph/file/683534e99d3ddd0b56aa9.jpg",
+ "name": "افريقيا"
+        },
+    {
+  "img": "https://telegra.ph/file/991dbe9ea353649960fe1.jpg",
+ "name": "تركيا"
+        },
+    {
+  "img": "https://telegra.ph/file/0fd8145f04feeab9a0c66.jpg",
+ "name": "نيجيريا"
+        },
+    {
+  "img": "https://telegra.ph/file/42bc49b92dac7a7f7d3a6.jpg",
+ "name": "كوريا الشماليه"
+        },
+    {
+  "img": "https://telegra.ph/file/6c0df8c1c1d8fc02b1484.jpg",
+ "name": "تونس"
+        },
+    {
+  "img": "https://telegra.ph/file/961ad1b0cf2d42545526f.jpg",
+ "name": "انجلترا"
+        },
+    {
+  "img": "https://telegra.ph/file/6ce27379ecdc38798fa6a.jpg",
+ "name": "فنلندا"
+         },
+    {
+  "img": "https://telegra.ph/file/3823f6f920b0232ddd581.jpg",
+ "name": "تشيلي"
+         },
+    {
+  "img": "https://telegra.ph/file/e6a96b0a577138c9c8b2a.jpg",
+ "name": "فنزويلا"
+        },
+    {
+  "img": "https://telegra.ph/file/6a491163f18644767dcba.jpg",
+ "name": "باكستان"
+        },
+    {
+  "img": "https://telegra.ph/file/14f9b34032d9552ece68b.jpg",
+ "name": "الكويت"
+        },
+    {
+  "img": "https://telegra.ph/file/6471474c2d3e8dbec2ce9.jpg",
+ "name": "فلسطين"
+        },
+    {
+  "img": "https://telegra.ph/file/5cf43c3b6c6c22255874d.jpg",
+ "name": "السودان"
+        },
+    {
+  "img": "https://telegra.ph/file/d798fd9ef1baaab66077a.jpg",
+ "name": "اوروبا"
+ }
+]
